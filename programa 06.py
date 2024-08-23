1cbd151e552aadd62d022e4e16354fbd58421661
caarloos82 <carlosghcr02@gmail.com>
2024-08-23 18:19:49 +0000

    respuesta coherente

---
+++ programa 06.py
@@ -0,0 +1,9 @@
+#vamos a por un 6to programa
+
+import random
+a = input("¿Cómo te sientes? ")
+lista = ["bien","genial","fenomenal"]
+if a in lista:
+    print("Cuánto me alegro!!!")
+else:
+    print("Vaya no sé qué decirte")
