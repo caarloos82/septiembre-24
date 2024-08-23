e02564cb335cbc73d4e71c667b988b37a329e07f
caarloos82 <carlosghcr02@gmail.com>
2024-08-23 18:00:57 +0000

    2024.08.23

---
+++ programa 05 magic 8 ball.py
@@ -0,0 +1,7 @@
+#Programa magic 8 ball
+import random
+a = input("Haz una pregunta sí o no: ")
+lista = ["sí","no","puede que no","seguramente sí","eso está hecho","100% seguro que sí","sinceramente no creo"]
+i = random.randint(0,len(lista))
+print(lista[i])
+
