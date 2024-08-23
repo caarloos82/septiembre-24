292b6097383850e21ff01d01a9e8a75844e5e839
caarloos82 <carlosghcr02@gmail.com>
2024-08-23 17:45:39 +0000

    programas añadidos 03 y 04

---
+++ programa 03.py
@@ -0,0 +1,3 @@
+import random
+print("el número premiado es el")
+print(random.randint(0,10))
---
+++ programa 04.py
@@ -0,0 +1,6 @@
+#programa 03 divisiones
+
+a = int(input("Escribe un número"))
+b = int(input("Escribe otro número"))
+c = a/b
+print("la división del primer número entre el segundo es",c)
